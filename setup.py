fee5f2e5c36e6da550477cb2be31bb33a5a2af21
Felix Kaiser <felix.kaiser@fxkr.net>
2013-10-07 17:44:14 +0200

    Add PKGBUILD and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='eagle-diff',
+    version='0.0.1',
+    description='Show differences between Cadsoft Eagle files',
+    author='Felix Kaiser',
+    author_email='felix.kaiser@fxkr.net',
+    long_description=open('README.md').read(),
+    license=open('LICENSE').read(),
+    url='https://github.com/fxkr/eagle-diff',
+    packages=[],
+    scripts=['eagle-diff'])
+
